366a299fa47fd9a6ef46e8264b09c596b4574588
mamaxis-design <snsstartdiary@gmail.com>
2026-08-01 18:27:35 +0900

    文言の揺らぎや余白調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.itpc.co.jp',
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap({
+      filter: (page) => !page.includes('/thanks'),
     }),
   ],
 });
